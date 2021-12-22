591842b5490074c7b6b41899af2ec1e948935200
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-22 14:10:31 +0500

    Update get_model.py

--- models/get_model.py
+++ models/get_model.py
@@ -5,5 +5,6 @@ import os
 if os.path.isfile('arcface.onnx'):
   print("File 'arcface.onnx' already exists")
 else:
-  subprocess.run(['wget', '-O', 'arcface.onnx', 'https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing'])
+  subprocess.run(['gdown', '--id' '16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr'])
+  #subprocess.run(['wget', '-O', 'arcface.onnx', 'https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing'])
   #! wget -O arcface.onnx "https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing"
