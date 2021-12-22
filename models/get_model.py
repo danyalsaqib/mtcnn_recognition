3a609389256f186774e378779269cf95e14b34bb
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-22 11:45:51 +0500

    Create get_model.py

---
+++ models/get_model.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+# Downloading pretrained models
+if os.path.isfile('arcface.onnx'):
+  print("File 'arcface.onnx' already exists")
+else:
+  subprocess.run(["wget", "-O arcface.onnx", "'https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing'"])
+  #! wget -O arcface.onnx "https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing"
