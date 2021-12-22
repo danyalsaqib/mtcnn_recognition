e70b9c79b74d33ecd86c7c7dbe6e8eb1c7614bd4
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-22 14:52:34 +0500

    Update get_model.py

--- face_recognition/models/get_model.py
+++ face_recognition/models/get_model.py
@@ -7,6 +7,6 @@ if os.path.isfile('arcface.onnx'):
 else:
   os.chdir("face_recognition/models")
   subprocess.run(['gdown', '--id', '16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr'])
-  os.chdir("..")
+  os.chdir("../..")
   #subprocess.run(['wget', '-O', 'arcface.onnx', 'https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing'])
   #! wget -O arcface.onnx "https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing"
