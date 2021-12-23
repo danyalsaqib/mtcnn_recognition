8fe5b688f7cb20bcbfed64736ba25fb479eb6d9c
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-23 16:07:45 +0500

    Update get_model.py

--- face_recognition/models/get_model.py
+++ face_recognition/models/get_model.py
@@ -5,8 +5,8 @@ import os
 if os.path.isfile('arcface.onnx'):
   print("File 'arcface.onnx' already exists")
 else:
-  os.chdir("face_recognition/models")
+  os.chdir("models")
   subprocess.run(['gdown', '--id', '16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr'])
-  os.chdir("../..")
+  os.chdir("..")
   #subprocess.run(['wget', '-O', 'arcface.onnx', 'https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing'])
   #! wget -O arcface.onnx "https://drive.google.com/file/d/16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr/view?usp=sharing"
