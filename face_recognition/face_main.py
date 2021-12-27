f482728699067837b87f17af82d7118fca0e8985
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-27 16:51:57 +0500

    Update face_main.py

--- face_recognition/face_main.py
+++ face_recognition/face_main.py
@@ -27,11 +27,11 @@ tform = trans.SimilarityTransform()
 distance_threshold = 0.5
 
 file_batch_dict = {
-    "frame_1" : "images/image_1.jpeg",
-    "frame_2" : "images/image_2.jpeg",
-    "frame_3" : "images/image_3.jpeg",
-    "frame_4" : "images/image_4.jpeg",
-    "frame_5" : "images/image_5.jpeg"
+    "frame_1" : "face_recognition/images/image_1.jpeg",
+    "frame_2" : "face_recognition/images/image_2.jpeg",
+    "frame_3" : "face_recognition/images/image_3.jpeg",
+    "frame_4" : "face_recognition/images/image_4.jpeg",
+    "frame_5" : "face_recognition/images/image_5.jpeg"
 }
 
 def face_infer_batch(batch_dict):
